e2f9682068d734fc8a95a86dda842114592d30cd
hiukongDan <ifsoboy9148@hotmail.com>
2022-05-20 17:07:37 +0800

    create directories for app/user and app/data in install.cmd

--- install.cmd
+++ install.cmd
@@ -4,5 +4,7 @@ pip install eel
 pip install openpyxl
 pip install bcrypt
 echo Install required modules successfully!
+mkdir app/user
+mkdir app/data
 echo python main.py > junting.cmd
 powershell -File shortcut.ps1
